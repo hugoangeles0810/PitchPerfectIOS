ab4f734d381c0f39387ac86d88fc09f36d1d43af
Hugo Angeles <hugoangeles0810@gmail.com>
2017-10-26 22:06:06 -0400

    learning ibaction and iboutlet

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblRecordingStatus: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        lblRecordingStatus.text = "Recording in progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        lblRecordingStatus.text = "Tap to recording"
+    }
 }
 
